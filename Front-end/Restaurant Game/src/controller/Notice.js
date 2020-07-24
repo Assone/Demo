1ecddfc2b1aacb3f7c447043b7f0326c5da806a2
Reset0 <reset0@Reset0deMacBook-Air.local>
2020-07-24 18:49:08 +0800

    添加notice的view和controller

---
+++ Front-end/Restaurant Game/src/controller/Notice.js
@@ -0,0 +1,7 @@
+import Controller from './Controller';
+
+export default class NoticeController extends Controller {
+  constructor(view, model) {
+    super(view, model);
+  }
+}
--- Front-end/Restaurant Game/src/view/Notice.js
+++ Front-end/Restaurant Game/src/view/Notice.js
@@ -8,4 +8,44 @@ export default class NoticeView extends View{
   getComponent() {
     this._component.welcome = this._el.querySelector('#welcome');
   }
+
+  exec() {
+    this.update.welcome();
+  }
+
+  bindEvent() {
+    this._component.welcome.addEventListener('click', evt => {
+      const dir = evt.target.dataset.dir
+      
+      if (dir === 'start') {
+        this.update.welcome(); // 隐藏欢迎页面
+        localStorage.setItem('showWelcome', false); // 设置下次不展示欢迎页面
+      }
+    })
+  }
+
+  get methods() {
+    return {
+      switchClass(node, className) {
+        node.classList.contains(className) ? node.classList.remove(className) : node.classList.add(className);
+      }
+    }
+  }
+
+  get update() {
+    let _this = this;
+
+    return {
+      welcome() {
+        let state = localStorage.getItem('showWelcome');
+
+        // 如果之前显示过了欢迎页面则不再显示
+        if (state === 'false') return;
+
+        // 切换notice和welcome的样式
+        _this.methods.switchClass(_this._el, 'show');
+        _this.methods.switchClass(_this._component.welcome, 'show');
+      }
+    }
+  }
 }
