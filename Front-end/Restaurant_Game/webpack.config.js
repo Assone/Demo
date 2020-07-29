2f28e670f403d855a456c2abd9fceabca0f6c624
A3onny <a3onny@gmail.com>
2020-07-29 15:35:50 +0800

    格式化webpack配置文件

--- Front-end/Restaurant_Game/webpack.config.js
+++ Front-end/Restaurant_Game/webpack.config.js
@@ -1,23 +1,23 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: "./dist",
-    host: "0.0.0.0",
+    contentBase: './dist',
+    host: '0.0.0.0',
     port: 8080,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template/index.html",
+      template: './src/index.html',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
@@ -26,26 +26,23 @@ module.exports = {
     rules: [
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
+          'css-loader',
+          'sass-loader',
           {
-            loader: "sass-resources-loader",
+            loader: 'sass-resources-loader',
             options: {
               resources: [
-                path.resolve(
-                  __dirname,
-                  "./src/assets/style/common.scss"
-                ),
+                path.resolve(__dirname, './src/assets/style/common.scss'),
               ],
             },
           },
