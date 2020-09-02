36fea987200911987a232cb3ec188f29cfcf7cd5
A3onny <a3onny@gmail.com>
2020-09-03 01:43:29 +0800

    去除在result里重置grid list的逻辑并转移至App

--- Front-end/vue__minesweeper/src/App.vue
+++ Front-end/vue__minesweeper/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <button id="app__setting"></button>
-    <Menu />
+    <button id="app__setting" @click="triggerMenu"></button>
+    <Menu v-if="showMenu" />
     <div id="app__container">
-      <Result />
-      <Container />
+      {{ count.steps }}{{ count.flag }}
+      <Result @click.native="resetGame" />
+      <Container ref="container" :key="id" />
       <!-- <Info /> -->
     </div>
   </div>
@@ -12,7 +13,8 @@
 
 <script>
 import { Component, Vue } from 'vue-property-decorator';
-// import { State, Mutation } from 'vuex-class';
+import { State, Mutation } from 'vuex-class';
+
 import Result from '@/components/Result.vue';
 import Container from '@/components/Container.vue';
 // import Info from '@/components/Info/index.vue';
@@ -27,14 +29,20 @@ import Menu from '@/components/Menu.vue';
   },
 })
 export default class App extends Vue {
-  // @State('grid') list;
-  // @Mutation('getGrid') start;
-  // get grid() {
-  //   return this.list;
-  // }
-  // created() {
-  //   this.start();
-  // }
+  @State count;
+  @Mutation('getGrid') start;
+
+  id = 0;
+  showMenu = false;
+
+  triggerMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  resetGame() {
+    this.start();
+    this.id++;
+  }
 }
 </script>
 
