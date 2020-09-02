3fd4da5dad76dd132ce4ddb24d905ddd56db2b79
A3onny <a3onny@gmail.com>
2020-09-02 18:59:45 +0800

    将state部分数据迁移到module前备份

--- Front-end/vue__minesweeper/src/store/index.js
+++ Front-end/vue__minesweeper/src/store/index.js
@@ -8,11 +8,41 @@ export default new Vuex.Store({
     row: 10,
     col: 10,
     bombs: 10,
+    grid: [],
+    status: {
+      isWin: false,
+      gameOver: false,
+    },
+    emoji: {
+      nor: '😀',
+      win: '😎',
+      loser: '😵',
+      empty: '🐣',
+      bomb: '💣',
+      flag: '🚧',
+      starter: '◻️',
+      numbers: ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣'],
+    },
   },
   getters: {},
   mutations: {
     setSize(state, { type, value }) {
       state[type] = Number(value);
+    },
+    getGrid(state) {
+      const { row, col, bombs } = state;
+
+      const temp = [
+        ...new Array(row * col - bombs).fill(false),
+        ...new Array(bombs).fill(true),
+      ].sort(() => 0.5 - Math.random());
+      const arr = [];
+
+      for (let i = 0; i < temp.length; i += col) {
+        arr.push(temp.slice(i, i + col));
+      }
+
+      state.grid = arr;
     },
   },
   actions: {
